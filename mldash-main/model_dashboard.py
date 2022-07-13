d55f52792dfee2469f9d3a9c7478eca1df528c7c
luca.mangini <luca.mangini.98@gmail.com>
2022-07-13 11:14:56 +0200

    Update model_dashboard.py

--- mldash-main/model_dashboard.py
+++ mldash-main/model_dashboard.py
@@ -14,18 +14,18 @@ garden = st.number_input("Y a un jardin")
 
 model = load_model()
 
-if taille_ok <= 0:
+if taille <= 0:
     st.write('mettre taille correcte')
 
-if nb_rooms_ok <= 0:
+if nb_rooms <= 0:
     st.write("mettre nombre de chambre correct")
 
-if garden_ok < 0:
+if garden < 0:
     st.write("mettre nombre de jardin correct")
 
-if taille_ok > 0 and nb_room_ok > 0:
+if taille > 0 and nb_rooms > 0:
         
-    X = [[taille_ok, nb_room_ok, nb_garden_ok]]
+    X = [[taille, nb_rooms, garden]]
     prediction = model.predict(X)
     ## afficher la prediction
     st.write("le prix de la maison est : {}". format(prediction[0]))
